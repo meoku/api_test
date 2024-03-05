079c2cce1d3ddd48393da6bb176c71b5172bf5d9
JSL107 <juneseok0107@naver.com>
2024-03-05 17:47:53 +0900

    feat: Create and Divide Function WeatherAPI
    스케줄러로 작동할 함수와 JSON parsing 하는 함수 분리

--- api_server/src/main/java/com/upgrade/apiserver/weather/WeatherService.java
+++ api_server/src/main/java/com/upgrade/apiserver/weather/WeatherService.java
@@ -1,11 +1,18 @@
 package com.upgrade.apiserver.weather;
 
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+
 public interface WeatherService {
 
     /*
      * @param lat : 예보 지점 X 좌표
      * @param lon : 예보 지점 y 좌표
      * */
-    String getWeatherDataToAPI(String lat, String lon);
+    // TODO :: 배치로 구현하기
+    void getWeatherDataToAPI(String lat, String lon);
 
+    void addWeatherDataInDB(@RequestBody String dto);
 }
