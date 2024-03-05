e3ef5fe790047130c1a8ebe4a0140e3bfe555d05
JSL107 <juneseok0107@naver.com>
2024-03-05 17:46:18 +0900

    fix: fix WeatherEntity Autowired
    Weather Entity를 Autowired함으로써 생긴 버그 처리

--- api_server/src/main/java/com/upgrade/apiserver/weather/WeatherController.java
+++ api_server/src/main/java/com/upgrade/apiserver/weather/WeatherController.java
@@ -1,6 +1,7 @@
 package com.upgrade.apiserver.weather;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class WeatherController {
 
     private WeatherService weatherService;
@@ -15,9 +17,8 @@ public class WeatherController {
     private WeatherEntity weather;
 
     @Autowired
-    public WeatherController(WeatherService weatherService, WeatherEntity weather) {
+    public WeatherController(WeatherService weatherService) {
         this.weatherService = weatherService;
-        this.weather = weather;
     }
 
     @GetMapping("/weather")
@@ -28,8 +29,9 @@ public class WeatherController {
     //37.549328709, 126.913624675 : 합정역 좌표
     // TODO :: DB에 집어넣기
     @GetMapping("/api/weather")
-    public void getWeatherDataToAPI() {
-        String response = weatherService.getWeatherDataToAPI("37.549328709", "126.913624675");
+    public String getWeatherDataToAPI() {
+        weatherService.getWeatherDataToAPI("37.549328709", "126.913624675");
+        return "OK";
     }
 
     // TODO :: DB에서 가져오기
