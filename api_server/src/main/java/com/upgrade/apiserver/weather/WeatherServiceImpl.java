7a9e46f464d0dd43d55f486f8678bb54e6c45529
JSL107 <juneseok0107@naver.com>
2024-03-05 17:49:28 +0900

    feat: Create Function implements WeatherService
    스케줄러 동작 함수 기능을 외부 API와 연동하여 가져오는 함수 구현 및 가져온 JSON데이터를 형태에 맞게 Parsing하는 함수 구현

--- api_server/src/main/java/com/upgrade/apiserver/weather/WeatherServiceImpl.java
+++ api_server/src/main/java/com/upgrade/apiserver/weather/WeatherServiceImpl.java
@@ -1,45 +1,106 @@
 package com.upgrade.apiserver.weather;
 
-
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Calendar;
+import java.util.Date;
 
 import static java.net.http.HttpClient.*;
 
 @Service
+@Slf4j
 public class WeatherServiceImpl implements WeatherService {
+
 
     @Value("${WEATHER_API_KEY}")
     private String appId;
     String lang = "kr";
-    String exclude = "daily";
     String units = "metric";
 
-//    https://api.openweathermap.org/data/3.0/onecall?lang=kr&exclude=hourly&units=metric
 
-    // MEMO :: 검색기능을 넣는게 맞나?
+    // TODO :: 배치로 구현하기 (09:00 ~ 20:00)
     @Override
-    public String getWeatherDataToAPI(String lat, String lon) {
-        final String url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + appId + "&lang=" + lang + "&exclude=" + exclude + "&units=" + units;
+    public void getWeatherDataToAPI(String lat, String lon) {
+
+        final String url = MessageFormat.format("https://api.openweathermap.org/data/3.0/onecall?lat={0}&lon={1}&appid={2}&lang={3}&units={4}", lat, lon, appId, lang, units);
+        HttpClient client = newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response != null) {
+                addWeatherDataInDB(response.body());
+            }
+        } catch (Exception e) {
+            log.error("error log = {}", "response is null (1. 파라 미터 값 체크 or 2. API 호출 횟수 체크)");
+        }
+    }
+
+    @Override
+    public void addWeatherDataInDB(@RequestBody String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        LocalDate today = LocalDate.now();
+        LocalTime noon = LocalTime.NOON;
+        LocalDateTime dateTime = LocalDateTime.of(today, noon);
+        ZoneId zoneId = ZoneId.of("Asia/Seoul");
+        Object millis = dateTime.atZone(zoneId).toInstant().toEpochMilli() / 1000;
 
         try {
-            HttpClient client = newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url))
-                    //MEMO :: HTTP METHOD 정하는 방법
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            JSONObject jsonData = (JSONObject) jsonParser.parse(jsonString);
+            JSONObject current = (JSONObject) jsonData.get("current");
+            JSONObject weatherData = (JSONObject) jsonParser.parse(current.toJSONString());
+            JSONArray weatherArray = (JSONArray) weatherData.get("weather");
+            JSONObject weather = (JSONObject) weatherArray.get(0);
 
-        } catch (InterruptedException | IOException e) {
-            return e.toString();
-        } finally {
-            //
+            Object description = weather.get("description");
+            Object temp = current.get("temp");
+            Object uvi = current.get("uvi");
+
+            //daily.dt>=date
+            //daily.min minTemp
+            //daily.max maxTemp
+
+            JSONObject dailyData = (JSONObject) jsonParser.parse(jsonData.toJSONString());
+            JSONArray dailyArray = (JSONArray) dailyData.get("daily");
+            JSONObject daily;
+            JSONObject dailyTemp;
+
+            Object dt;
+            Object maxTemp;
+            Object minTemp;
+
+            for (int i = 0; i < dailyArray.size(); i++) {
+                daily = (JSONObject) dailyArray.get(i);
+                dt = daily.get("dt");
+                System.out.println(dt.equals(millis));
+
+                if (dt.equals(millis)) {
+                    dailyTemp = (JSONObject) daily.get("temp");
+                    minTemp = dailyTemp.get("min");
+                    maxTemp = dailyTemp.get("max");
+                }
+            }
+
+
+        } catch (ParseException e) {
+            log.error("error log = {}", e.toString());
+
         }
+
     }
 }
