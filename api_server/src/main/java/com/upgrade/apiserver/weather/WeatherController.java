fddf016cfa94310b3c3901926c49b9a302bf20c3
JSL107 <juneseok0107@naver.com>
2024-03-24 21:21:32 +0900

    docs: 날씨 API 사용 문서 작성
    날씨 API에 요청 하는 방식 문서로 정리

--- api_server/src/main/java/com/upgrade/apiserver/weather/WeatherController.java
+++ api_server/src/main/java/com/upgrade/apiserver/weather/WeatherController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-
-
 @RestController
 @RequiredArgsConstructor
 @Slf4j
