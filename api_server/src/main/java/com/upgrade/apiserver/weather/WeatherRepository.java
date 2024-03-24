10062ae85796b329fcb74c2f459c24e65c3fd530
JSL107 <juneseok0107@naver.com>
2024-03-24 17:02:01 +0900

    feat: DB에 Parsing된 데이터 저장을 위한 Repository생성
    Hibernate를 상속받는 Repository 생성 테스트 Repository코드인 findByLocation 생성

---
+++ api_server/src/main/java/com/upgrade/apiserver/weather/WeatherRepository.java
@@ -0,0 +1,9 @@
+package com.upgrade.apiserver.weather;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WeatherRepository extends JpaRepository<WeatherEntity, Long> {
+    List<WeatherEntity> findByLocation(String location);
+}
